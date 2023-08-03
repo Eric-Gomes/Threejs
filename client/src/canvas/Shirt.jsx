f709242b8760e5dbfe10c7488c314852a53e1f5a
Eric Gomes <eric.n.gomes@hotmail.com>
2023-08-03 12:22:50 +0100

    Added functionality for shirt logo and decal through static changes to test

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -12,6 +12,9 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  //return function for mesh showing the t shirt then check if showing full texture
+  // or showing full logo 
+
   return (
     <group>
       <mesh
@@ -21,6 +24,23 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0,0,0]}
+            rotation={[0,0,0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal 
+            position={[0,0.04,0.15]}
+            rotation={[0,0,0]}
+            scale={0.15}
+            map={logoTexture}
+          />
+        )}
       </mesh>
     </group>
   )

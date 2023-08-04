3c3d8994ea8f10123a7daaaea3730e19fbb11159
Eric Gomes <eric.n.gomes@hotmail.com>
2023-08-04 12:00:35 +0100

    setting buttons for color picker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -31,7 +31,11 @@ const generateTabContent = () => {
     case "colorpicker":
       return <ColorPicker />
     case "filepicker":
-      return <FilePicker />
+      return <FilePicker 
+        file={file}
+        setFile={setFile}
+        readFile={readFile}
+      />
     case "aipicker":
       return <AiPicker />
   
@@ -39,6 +43,39 @@ const generateTabContent = () => {
       return null;
   }
 }
+
+const handleDecals = (type, result) => {
+  const decalType = DecalTypes[type];
+
+  state[decalType.stateProperty] = result;
+
+  if(!activeFilterTab[decalType.filterTab]) {
+    handleActiveFilterTab(decalType.filterTab)
+  }
+}
+
+const handleActiveFilterTab = (tabName) => {
+  switch (tabName) {
+    case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+      break;
+    case "stylishShirt":
+      state.isFullTexture = !activeFilterTab[tabName];
+    default:
+      state.isLogoTexture = true;
+      state.isFullTexture = false;
+      break;
+  }
+}
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })
+  }
+
 
   return (
     <AnimatePresence>
